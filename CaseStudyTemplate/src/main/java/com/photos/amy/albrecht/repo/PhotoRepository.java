9fda0f4d0929e0baa147bc5dc5d9d0e8eb674b1e
Amy Albrecht <albrechtamye@gmail.com>
2021-04-02 16:55:42 -0500

    created repo and created PhotoRepository

---
+++ CaseStudyTemplate/src/main/java/com/photos/amy/albrecht/repo/PhotoRepository.java
@@ -0,0 +1,12 @@
+package com.photos.amy.albrecht.repo;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.photos.amy.albrecht.entities.Photo;
+
+@Repository
+public interface PhotoRepository extends JpaRepository<Photo, Integer> {
+	Photo getPhotoByPhotoId(int id);
+	
+}
